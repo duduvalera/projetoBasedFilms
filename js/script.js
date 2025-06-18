9cef73a8903ce13e1485e98614099575654bd80b
Manuka <emanuel.v3@protonmail.com>
2025-06-17 21:28:06 -0300

    finalizado filtros

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
+function filterMovies(category) {
+  let arrayFilmesGenero = listaFilmes.filter((filme) => {
+    if (category === "todos") {
+      return filme;
+    } else {
+      return filme.genero === category;
+    }
+  });
+
+  return arrayFilmesGenero;
+}
+
 function initMovieFilter() {
   const menuItens = document.querySelectorAll(".menu-principal .item");
-
-  function filterMovies(category) {
-    let arrayFilmesGenero = listaFilmes.filter((filme) => {
-      if (category === "todos") {
-        return filme;
-      } else {
-        return filme.genero === category;
-      }
-    });
-    console.log(arrayFilmesGenero);
-    carregarFilmes(arrayFilmesGenero);
-  }
 
   menuItens.forEach((menuItem) => {
     menuItem.addEventListener("click", function (event) {
       event.preventDefault();
 
       const movieCategory = event.target.dataset.category;
-      filterMovies(movieCategory);
+      const inputHiddenPesquisa = (document.querySelector("#categoria").value =
+        movieCategory);
+
+      let arrayFiltrado = filterMovies(movieCategory);
+      carregarFilmes(arrayFiltrado);
     });
   });
 }
@@ -112,6 +116,72 @@ function verificarCarregamentodaPagina() {
 
 verificarCarregamentodaPagina();
 
+const filtrosPesquisa = document.querySelector("#filtros-pesquisa");
+
+function converterDuracaoParaMinutos(duracao) {
+  const horasMatch = duracao.match(/(\d+)h/);
+  const minutosMatch = duracao.match(/(\d+)m/);
+
+  const horas = horasMatch ? parseInt(horasMatch[1], 10) : 0;
+  const minutos = minutosMatch ? parseInt(minutosMatch[1], 10) : 0;
+
+  return horas * 60 + minutos;
+}
+
+filtrosPesquisa.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const dadosFormulario = new FormData(filtrosPesquisa);
+  let ordemAlfabetica = dadosFormulario.get("ordem-alfabetica");
+  let ordemAvaliacao = dadosFormulario.get("nota");
+  let ordemDuracao = dadosFormulario.get("duracao");
+  let ordemClassificacao = dadosFormulario.get("classificacao");
+  let arrayFormulario = filterMovies(dadosFormulario.get("categoria"));
+  // const filmeCurto = ordemDuracao
+  if (ordemAlfabetica === "asc") {
+    arrayFormulario.sort((a, b) => {
+      if (a.titulo < b.titulo) return -1;
+      if (a.titulo > b.titulo) return 1;
+      return 0;
+    });
+  } else {
+    arrayFormulario.sort((a, b) => {
+      if (a.titulo > b.titulo) return -1;
+      if (a.titulo < b.titulo) return 1;
+      return 0;
+    });
+  }
+  if (ordemAvaliacao === "mais-avaliados") {
+    arrayFormulario.sort((a, b) => {
+      if (a.nota > b.nota) return -1;
+      if (a.nota < b.nota) return 1;
+      return 0;
+    });
+  } else {
+    arrayFormulario.sort((a, b) => {
+      if (a.nota < b.nota) return -1;
+      if (a.nota > b.nota) return 1;
+      return 0;
+    });
+  }
+
+  arrayFormulario = arrayFormulario.filter((filme) => {
+    let duracaoEmMinutos = converterDuracaoParaMinutos(filme.duracao);
+    if (ordemDuracao === "curto") return duracaoEmMinutos <= 99;
+    else if (ordemDuracao === "medio")
+      return duracaoEmMinutos >= 100 && duracaoEmMinutos <= 140;
+    else return duracaoEmMinutos > 140;
+  });
+
+  arrayFormulario = arrayFormulario.filter((filme) => {
+    if (ordemClassificacao === "livre") {
+      return filme.classificacao === 0;
+    }
+    return +ordemClassificacao === filme.classificacao;
+  });
+  console.log(arrayFormulario);
+  carregarFilmes(arrayFormulario);
+});
+
 function Filmes(titulo, capa, nota, ano, duracao, classificacao, genero) {
   this.titulo = titulo;
   this.capa = capa;
@@ -146,14 +216,14 @@ const listaFilmes = [
     "./img/interestelar.jpg",
     8.1,
     1976,
-    "2h",
-    16,
+    "2h40m",
+    0,
     "drama"
   ),
   new Filmes(
     "O Cavaleiro das trevas",
     "./img/batmanct1.jpg",
-    9.4,
+    8.8,
     1976,
     "2h30m",
     14,
@@ -164,7 +234,7 @@ const listaFilmes = [
     "./img/rocky.jpg",
     8.1,
     1976,
-    "2h",
+    "3h",
     16,
     "fantasia"
   ),
@@ -173,7 +243,7 @@ const listaFilmes = [
     "./img/umsonhodeliberdade.jpg",
     9.4,
     1976,
-    "2h30m",
+    "1h40m",
     14,
     "comedia"
   ),
@@ -182,14 +252,14 @@ const listaFilmes = [
     "./img/interestelar.jpg",
     8.1,
     1976,
-    "2h",
+    "1h20m",
     16,
     "drama"
   ),
   new Filmes(
-    "O Cavaleiro das trevas",
+    "Seven",
     "./img/batmanct1.jpg",
-    9.4,
+    7.7,
     1976,
     "2h30m",
     14,
